82403a9a4edac77d503dd670d6c52a2ac9c79af8
Jon-Martin Heiberg <heijon17@student.westerdals.no>
2019-12-03 14:33:56 +0100

    removed uneeded code

--- TopMusic/Model/Track.swift
+++ TopMusic/Model/Track.swift
@@ -68,13 +68,5 @@ struct Track: Decodable, Loadable {
         self.strThumb = try values.decode(String?.self, forKey: .strTrackThumb)
     }
     
-    public init(artist: String, trackId: String, name: String, albumId: String, album: String, duration: String, thumb: String?) {
-        strArtist = artist
-        idTrack = trackId
-        strName = name
-        idAlbum = albumId
-        strAlbum = album
-        intDuration = duration
-        strThumb = thumb
-    }
+
 }
